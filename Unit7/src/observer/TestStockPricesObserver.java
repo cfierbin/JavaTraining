8ef125884808d0371ccd1d775729d487c216d6fb
Cristina Fierbinteanu <cristina.fierbinteanu@gmail.com>
2014-03-17 13:34:17 +0200

    added test for observable stock prices panel

--- Unit7/src/observer/TestStockPricesObserver.java
+++ Unit7/src/observer/TestStockPricesObserver.java
@@ -1,18 +1,42 @@
 package observer;
 
+import java.util.TimerTask;
+
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 
 public class TestStockPricesObserver {
+	
+	public static enum StockSymbol {AAPL, ORCL, IBM, AMZN, FB, MSFT, MSI}
+	
+	private static StockSymbol symbol;
+	private static int price;
+	
+    private static java.util.Random randomInstance = new java.util.Random();
+    
+	static private void getMarketNews() {
+		symbol = StockSymbol.values()[getRandomSymbol()];	
+		price = getRandomPrice();
+	}
+	
+	static private int getRandomPrice() {
+		return randomInstance.nextInt(100);		
+	}
+	
+	static private int getRandomSymbol() {
+		return randomInstance.nextInt(7);	
+	}
 
 	public static void main(String[] args) {
+		//create timer
+		java.util.Timer timer = new java.util.Timer();
 		
 		// create frames
 		JFrame frame1 = new JFrame("Stock Prices Panel");
 		JFrame frame2 = new JFrame("Trading Panel");
 		
 		// create a StockPricesPanel
-		StockPricesPanel stockPricesPanel = new StockPricesPanel();
+		final StockPricesPanel stockPricesPanel = new StockPricesPanel();
 		// add layout to StockPricesPanel
 		stockPricesPanel.setLayout(new BoxLayout(stockPricesPanel,BoxLayout.Y_AXIS));
 		// add StockPricesPanel to JFrame
@@ -32,7 +56,25 @@ public class TestStockPricesObserver {
 		frame2.setLocation(500, 200);
 		frame2.setSize(250,100);
 		frame2.setVisible(true);
+				
+        //generate stock symbols and prices
+		int delay = 0;
+		int period = randomInstance.nextInt(1000);
+		
+		timer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+            	TestStockPricesObserver.getMarketNews();
+            	stockPricesPanel.setStockSymbol(String.valueOf(symbol));
+            	stockPricesPanel.setPrice(String.valueOf(price));
+            }
+
+        }, delay, period);
+		
+
+    }
 
 	}
 
-}
+
