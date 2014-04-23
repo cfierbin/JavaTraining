1ffd5842072041c08e55748c6a5c0a01df1dc2dd
Cristina Fierbinteanu <cristina.fierbinteanu@gmail.com>
2014-04-23 21:08:05 +0300

    added try-with-resources sql exception handling in Lesson 22

--- Lesson22/src/Portfolio.java
+++ Lesson22/src/Portfolio.java
@@ -1,59 +1,90 @@
+
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-
 
 public class Portfolio implements Runnable {
 
 	@Override
-	public void run() {
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-		try {
+	public void run() {			
+		//conn = DriverManager.getConnection("jdbc:derby://localhost:1527/Lesson22");
 			
-			//conn = DriverManager.getConnection("jdbc:derby://localhost:1527/Lesson22");
+		//connect using DataSource 
+		org.apache.derby.jdbc.ClientDataSource ds = new org.apache.derby.jdbc.ClientDataSource();
+		ds.setServerName("localhost");
+		ds.setDatabaseName("Lesson22");
+		ds.setDescription("Sample database for Lesson 22");
 			
-			//connect using DataSource 
-			org.apache.derby.jdbc.ClientDataSource ds = new org.apache.derby.jdbc.ClientDataSource();
-			ds.setServerName("localhost");
-			ds.setDatabaseName("Lesson22");
-			ds.setDescription("Sample database for Lesson 22");
-			
-			conn = ds.getConnection();
-			
-			String sqlQuery = "SELECT * FROM PORTFOLIO";
-			stmt = conn.createStatement();
-			//Execute SQL and get the ResultSet object
-			rs = stmt.executeQuery(sqlQuery);
-			
-			//Process the result set
-			while(rs.next()){
-				String symbol = rs.getString("SYMBOL");
-				String quantity = rs.getString("QUANTITY");
-				double price = rs.getDouble("PRICE");
-				double totalPrice = price * Double.valueOf(quantity);
-				System.out.println(symbol + " quantity=" + quantity +
-						" price=" + totalPrice);
-			}
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		try (Connection conn = ds.getConnection();){
+				
+		//if table MYPORTFOLIO does not exist, create it	
+		//DatabaseMetaData example taken from http://content.hccfl.edu/pollock/AJava/DerbyDemo.htm
+		
+		String createTable = "CREATE TABLE myportfolio(" +
+					"id INT NOT NULL," +
+					"symbol VARCHAR(10) NOT NULL," +
+					"quantity VARCHAR(10) NOT NULL,"+
+					"price DOUBLE NOT NULL, PRIMARY KEY(id))";
+		
+		DatabaseMetaData dbmd = conn.getMetaData();
+		try (ResultSet rs = dbmd.getTables(null, null, "MYPORTFOLIO", null);
+			PreparedStatement	stmt = conn.prepareStatement(createTable);){
+					if(!rs.next())
+					{
+						System.out.println("Table does not exist. Creating table.");
+						stmt.execute();				
+					}
+		}catch (SQLException e) {
+				// TODO Auto-generated catch block
+				//	e.printStackTrace();
+			System.out.println("Table creation failed");
+				}
+				
+		//populate table myportfolio
+		String insertValues = "INSERT INTO myportfolio VALUES " +
+									"(1,'IBM','500',105.50)," +
+									"(2,'AMZN','1000',15.25)," +
+									"(3,'AAPL','2000', 272.50)";
+		try (PreparedStatement stmt = conn.prepareStatement(insertValues);)
+				{
+					stmt.execute();
+		}catch (SQLException e) {
+			System.out.println("Insert failed");
+				}
+				
+		//Execute SQL SELECT and get the ResultSet object		
+		String sqlQuery = "SELECT * FROM myportfolio";
+		try (PreparedStatement stmt = conn.prepareStatement(sqlQuery);
+			 ResultSet	rs = stmt.executeQuery();)
+		{
+					//Process the result set
+					while(rs.next()){
+						String symbol = rs.getString("symbol");
+						String quantity = rs.getString("quantity");
+						double price = rs.getDouble("price");
+						double totalPrice = price * Double.valueOf(quantity);
+						System.out.println(symbol + " quantity=" + quantity +
+								" price=" + totalPrice);
+					}
+					
+		}catch (SQLException e) {
+				System.out.println("SELECT failed");
 		}
-		finally{
-			try{
-				rs.close();
-				stmt.close();
-				conn.close();
-			}
-			catch(Exception e){
-				e.printStackTrace();
-			}
+							
+		//drop table MYPORTFOLIO
+		String dropTable = "DROP TABLE myportfolio";
+		try (PreparedStatement stmt = conn.prepareStatement(dropTable);){
+					stmt.execute();
+		}catch (SQLException e) {
+			System.out.println("Failed to drop table.");
 		}
-
+				
+} catch (SQLException e) {
+	System.out.println("Failed to open data source.");
 	}
+			
+}
 
 }
--- Lesson22/src/ShowMyPortfolio.java
+++ Lesson22/src/ShowMyPortfolio.java
@@ -6,10 +6,12 @@ public class ShowMyPortfolio {
 		Portfolio myPortfolio = new Portfolio();
 		Thread p = new Thread(myPortfolio, "Portfolio Data");
 		p.start();
-	
+		
+	/*uses Oracle Database 11g Express Edition
 		OracleHRSchemaDAO hr = new OracleHRSchemaDAO();
 		Thread o = new Thread(hr, "Country Data");
 		o.start();
+	*/
 		
 	}
 }
